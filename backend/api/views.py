4c33cef6f31271baeff1eef9253f21e0f9a8ff93
ark948 <armankarimi948@gmail.com>
2024-08-11 21:54:19 +0330

    Added api views and urls (to main urls.py file)

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.generics import CreateAPIView
+from api.serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Create your views here.
+
+class CreateUserView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = User
+    permission_classes = [AllowAny]
