480493764025d13015ee75586bddd43587d6e7e1
ark948 <armankarimi948@gmail.com>
2024-08-11 21:46:10 +0330

    Added User model serializer to api app

---
+++ backend/api/serializers.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from rest_framework.serializers import ModelSerializer
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "password"]
+        # do not return password when creating new users
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
