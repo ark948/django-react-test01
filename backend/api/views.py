fe0b0ba55d0520743fadc39729af9869a043c6c6
ark948 <armankarimi948@gmail.com>
2024-08-11 22:28:25 +0330

    Added api views for Note

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from rest_framework.generics import CreateAPIView
-from api.serializers import UserSerializer
+from rest_framework.generics import CreateAPIView, ListCreateAPIView, DestroyAPIView
+from api.serializers import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from api.models import Note
 
 # Create your views here.
+
+class NoteListCreate(ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+    
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+
+class NoteDelete(DestroyAPIView):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
 
 class CreateUserView(CreateAPIView):
     queryset = User.objects.all()
